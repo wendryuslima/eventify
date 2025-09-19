6f8a67d02f8f17c5362b1eee9ec615f87392470c
wendryuslima <141366401+wendryuslima@users.noreply.github.com>
2025-09-18 23:09:37 -0300

    fix: exclude backend folder from Next.js build and update config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: process.cwd(),
   output: "standalone",
-  experimental: {
-    outputFileTracingRoot: undefined,
-  },
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+  webpack: (config) => {
+    config.watchOptions = {
+      ignored: ["**/backend/**", "**/node_modules/**"],
+    };
+    return config;
   },
 };
 
